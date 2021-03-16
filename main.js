bc2ae27d0e47b9b02e71bf64ee1ec5725e92a34a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-16 16:35:13 +0200

    feat: Lock version

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { h, Component, render } from "https://unpkg.com/preact?module";
+import { h, Component, render } from "https://unpkg.com/preact@10.5.13?module";
 import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
