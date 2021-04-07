d23a323c0fdd4ae8c01b665d176aacf26b92a75b
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-07 20:43:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-import { h, Component, render } from "https://unpkg.com/preact@10.5.13?module";
 import htm from "https://unpkg.com/htm?module";
+import { h, render } from "https://unpkg.com/preact@10.5.13?module";
 
 const html = htm.bind(h);
 
 function App(props) {
-  return html`
-    <p>Hello, ${props.name}!</p>
-  `;
+  return html`<p>Hello, ${props.name}!</p>`;
 }
 
 render(html`<${App} name="developer" />`, document.getElementById("app"));
